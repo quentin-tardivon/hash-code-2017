4e4f896ede861e7f0e9183ac0950686711e06684
debilausaure <flovhs59@gmail.com>
2017-02-23 20:45:46 +0100

    Fixed Parser

--- parser.py
+++ parser.py
@@ -1,31 +1,35 @@
+def parse():
+    data = open("data.in", "r")
 
-data = open("data.in", "r")
-
-# First line parsing
-line = data.readline()
-V, E, R, C, X = line.split(" ")
-
-# Second line parsing
-line = data.readline()
-videosSize = line.split(" ")
-
-# Third part parsing
-endpointLatency = [[0 for x in range(E)] 0 for y in range(C+1)]
-for i in range(E):
+    # First line parsing
     line = data.readline()
-    LD , K = line.split(" ")
-    endpointLatency[i][0] = LD
-    for j in range(K):
+    V, E, R, C, X = line.split(" ")
+    V, E, R, C, X = int(V), int(E), int(R), int(C), int(X)
+
+    # Second line parsing
+    line = data.readline()
+    videosSize = line.split(" ")
+
+    # Third part parsing
+    endpointLatency = [[0 for x in range(C+1)] for y in range(E)]
+    for i in range(E):
         line = data.readline()
-        idC, LC = line.split(" ")
-        endpointLatency[i][j] = LC
+        LD , K = line.split(" ")
+        LD, K = int(LD), int(K)
+        endpointLatency[i][0] = LD
+        for j in range(K):
+            line = data.readline()
+            idC, LC = line.split(" ")
+            idC, LC = int(idC), int(LC)
+            endpointLatency[i][j] = LC
 
-#Fourth Part parsing
-videoRequestByEndpoint = [[0 for x in range(V)] 0 for y in range(E)]
-for i in range(R)
-    line = data.getline()
-    idV, idE, nbR = line.split(" ")
-    videoRequestByEndpoint[idV][idE] = nbR
+    #Fourth Part parsing
+    videoRequestByEndpoint = [[0 for x in range(E)] for y in range(V)]
+    for i in range(R):
+        line = data.readline()
+        idV, idE, nbR = line.split(" ")
+        idV, idE, nbR = int(idV), int(idE), int(nbR)
+        videoRequestByEndpoint[idV][idE] = nbR
 
-#Closing file
-data.close()
+    #Closing file
+    data.close()
